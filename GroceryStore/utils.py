785f0d97455e3fbc231fda5edf8cf0206e8477f3
XC <muhammedusman5c@gmail.com>
2025-03-27 15:17:43 +0500

    BugFix: in adding product fixed saleprice parameters

--- GroceryStore/utils.py
+++ GroceryStore/utils.py
@@ -9,13 +9,15 @@ def store_valid(store_name, password):
     return False
 
 def product_valid(quantity_field, saleprice_field, current_store, selected_item):
-    try:
-        quantity_float = float(quantity_field)
-        saleprice_decimal = Decimal(saleprice_field)
-        
-        if quantity_float.is_integer() and 1 <= int(quantity_float) <= 100 and 1.00 <= saleprice_decimal <= 20.00 and not Product.objects.filter(item_info=selected_item, store= current_store).exists():
-            return True
-        else:
-            return False  
-    except ValueError:
-        return False
+    if not saleprice_field == "" and not quantity_field == "":
+        try:
+            quantity_float = float(quantity_field)
+            saleprice_decimal = Decimal(saleprice_field)
+            
+            if quantity_float.is_integer() and 1 <= int(quantity_float) <= 100 and 1.00 <= saleprice_decimal <= 20.00 and not Product.objects.filter(item_info=selected_item, store= current_store).exists():
+                return True
+            else:
+                return False  
+        except ValueError:
+            return False
+    return False
